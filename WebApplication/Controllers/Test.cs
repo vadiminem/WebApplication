817d55d774964648ec0ffac16accb4aa77ac11e0
Вадим Волоткяйвичус <vadvc@yandex.ru>
2019-09-01 01:04:11 +0700

    Добавлен контроллер, который считывает данные и преобразует их в объект Добавлен json файл с данными в проект Добавлена модель объекта json

---
+++ WebApplication/Controllers/Test.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.IO;
+using System.Runtime.Serialization;
+using System.Runtime.Serialization.Json;
+using WebApplication.Models;
+
+namespace WebApplication.Controllers
+{
+    [Route("api/[controller]")]
+    public class Test : Controller
+    {
+        [HttpGet("GetData")]
+        public SomeObject GetData()
+        {
+            var path = "Data/test_set1.json";
+            var deserializedResult = new SomeObject();
+            if (System.IO.File.Exists(path))
+            {
+                var ms = new MemoryStream(System.IO.File.ReadAllBytes(path));
+                var dateFormatSettings = new DataContractJsonSerializerSettings
+                {
+                    DateTimeFormat = new System.Runtime.Serialization.DateTimeFormat("yyyy-MM-ddTH:mm:ss")
+                };
+                var ser = new DataContractJsonSerializer(deserializedResult.GetType(), dateFormatSettings);
+                deserializedResult = ser.ReadObject(ms) as SomeObject;
+                ms.Close();
+                Console.WriteLine(deserializedResult.timeGenerated);
+            }
+            return deserializedResult;
+        }
+    }
+}
---
+++ WebApplication/Models/SomeObject.cs
@@ -0,0 +1,55 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Converters;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Threading.Tasks;
+
+namespace WebApplication.Models
+{
+    [DataContract]
+    public class SomeObject
+    {
+        [DataMember]
+        public Result result;
+        [DataMember]
+        public string errorMessage { get; set; }
+        [DataMember]
+        public string timeGenerated { get; set; }
+    }
+
+    public class Result
+    {
+        [DataMember]
+        public List<Traks> traks;
+    }
+
+    public class Traks
+    {
+        [DataMember]
+        public List<Sets> sets;
+        [DataMember]
+        public int pos { get; set; }
+        [DataMember]
+        public int vMin { get; set; }
+        [DataMember]
+        public int vMax { get; set; }
+
+    }
+
+    public class Sets
+    {
+        [DataMember]
+        public List<Items> items;
+    }
+
+    public class Items
+    {
+        [DataMember]
+        //[JsonConverter(typeof(IsoDateTimeConverter))]
+        public DateTime time { get; set; }
+        [DataMember]
+        public float value { get; set; }
+    }
+}
