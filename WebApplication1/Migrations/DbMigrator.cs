2b361c80c8c17dc1286a7031500ee886ff0b60c5
Вадим Волоткяйвичус <vadvc@yandex.ru>
2019-09-27 22:57:25 +0700

    Добавлено создание бд при миграциях, если она отсутствует

--- WebApplication1/Migrations/DbMigrator.cs
+++ WebApplication1/Migrations/DbMigrator.cs
@@ -1,23 +1,45 @@
-﻿using ThinkingHome.Migrator;
+﻿using Npgsql;
+using System;
+using ThinkingHome.Migrator;
 
 namespace WebApplication1.Migrations
 {
     public class DbMigrator
     {
 
+        private string connectionStringWithDbName = default;
         private string connectionString = default;
-        public DbMigrator(string connectionString)
+        public DbMigrator(string connectionStringWithDbName, string connectionString)
         {
             this.connectionString = connectionString;
+            this.connectionStringWithDbName = connectionStringWithDbName;
         }
 
         public void StartMigration()
         {
-            var provider = "Postgres";
-            var assembly = typeof(DbMigration).Assembly;
-            using (var migrator = new Migrator(provider, connectionString, assembly))
+            try
             {
-                migrator.Migrate();
+                using (var connection = new NpgsqlConnection(connectionString))
+                {
+                    connection.Open();
+                    var createDb = new NpgsqlCommand(@"
+                    create database testwebapidb
+                    with owner = postgres
+                    encoding = 'utf-8'
+                    connection limit = -1;", connection);
+                    createDb.ExecuteNonQuery();
+                    connection.Close();
+                }
+            }
+            catch { }
+            finally
+            {
+                var provider = "Postgres";
+                var assembly = typeof(DbMigration).Assembly;
+                using (var migrator = new Migrator(provider, connectionStringWithDbName, assembly))
+                {
+                    migrator.Migrate();
+                }
             }
         }
     }
