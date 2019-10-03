0257a2819e97a0c6c6af83be29aa4afa0fb9da4e
Вадим Волоткяйвичус <vadvc@yandex.ru>
2019-10-03 10:50:39 +0700

    Удалено создание бд, если ее не существует Удалена лишняя строка с подключением к бд

--- WebApplication1/Migrations/DbMigrator.cs
+++ WebApplication1/Migrations/DbMigrator.cs
@@ -1,45 +1,22 @@
-﻿using Npgsql;
-using System;
-using ThinkingHome.Migrator;
+﻿using ThinkingHome.Migrator;
 
 namespace WebApplication1.Migrations
 {
     public class DbMigrator
     {
-
-        private string connectionStringWithDbName = default;
         private string connectionString = default;
-        public DbMigrator(string connectionStringWithDbName, string connectionString)
+        public DbMigrator(string connectionString)
         {
             this.connectionString = connectionString;
-            this.connectionStringWithDbName = connectionStringWithDbName;
         }
 
         public void StartMigration()
         {
-            try
+            var provider = "Postgres";
+            var assembly = typeof(DbMigration).Assembly;
+            using (var migrator = new Migrator(provider, connectionString, assembly))
             {
-                using (var connection = new NpgsqlConnection(connectionString))
-                {
-                    connection.Open();
-                    var createDb = new NpgsqlCommand(@"
-                    create database testwebapidb
-                    with owner = postgres
-                    encoding = 'utf-8'
-                    connection limit = -1;", connection);
-                    createDb.ExecuteNonQuery();
-                    connection.Close();
-                }
-            }
-            catch { }
-            finally
-            {
-                var provider = "Postgres";
-                var assembly = typeof(DbMigration).Assembly;
-                using (var migrator = new Migrator(provider, connectionStringWithDbName, assembly))
-                {
-                    migrator.Migrate();
-                }
+                migrator.Migrate();
             }
         }
     }
