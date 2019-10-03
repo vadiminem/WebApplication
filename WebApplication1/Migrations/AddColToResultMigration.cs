dbae43998720eeac35fcd5658633041a42358175
Вадим Волоткяйвичус <vadvc@yandex.ru>
2019-10-03 11:52:26 +0700

    Добавлена миграция в отдельном классе

---
+++ WebApplication1/Migrations/AddColToResultMigration.cs
@@ -0,0 +1,19 @@
+﻿using System.Data;
+using ThinkingHome.Migrator.Framework;
+
+namespace WebApplication1.Migrations
+{
+    [Migration(2)]
+    public class AddColToResultMigration : Migration
+    {
+        public override void Apply()
+        {
+            Database.AddColumn("result", new Column("date", DbType.DateTime));
+        }
+
+        public override void Revert()
+        {
+            Database.RemoveColumn("result", "date");
+        }
+    }
+}
